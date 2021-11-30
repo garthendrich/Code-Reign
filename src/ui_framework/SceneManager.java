a60e6720af60552907d77f467cb231a6a503e8c1
Garth Hendrich Lapitan <garthendrich@gmail.com>
2021-11-30 16:16:00 +0800

    Create SceneManager class

---
+++ src/ui_framework/SceneManager.java
@@ -0,0 +1,25 @@
+package ui_framework;
+
+import java.util.HashMap;
+
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class SceneManager {
+	private HashMap<String, View> routes = new HashMap<String, View>();
+	private Stage stage;
+
+	public SceneManager(Stage stage) {
+		this.stage = stage;
+	}
+
+	public void addRoute(String path, View view) {
+		routes.put(path, view);
+	}
+
+	public void load(String path) {
+		View view = routes.get(path);
+		Scene scene = view.getScene();
+		stage.setScene(scene);
+	}
+}
