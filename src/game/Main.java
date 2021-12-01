f9a6c3490affbd3bd71b467890da6def6f649a42
Garth Hendrich Lapitan <garthendrich@gmail.com>
2021-12-01 15:37:07 +0800

    Refactor GameStage

--- src/game/Main.java
+++ src/game/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
 
 	public void start(Stage stage){
 		GameStage theGameStage = new GameStage();
-		theGameStage.setStage(stage);
+		theGameStage.load(stage);
 	}
 
 }
