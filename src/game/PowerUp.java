d745682e072cf665236fbd0874cd6fe66b9a00a4
Alesundreau Dale Ratuiste <amratuiste@up.edu.ph>
2021-12-01 17:29:58 +0800

    Update PowerUp.java

--- src/game/PowerUp.java
+++ src/game/PowerUp.java
@@ -1,9 +1,11 @@
 package game;
 
+import javafx.scene.image.Image;
+
 abstract class PowerUp extends GameElement{
 
-	public PowerUp(int xPos, int yPos) {
-		super(xPos, yPos);
+	public PowerUp(int xPos, int yPos, Image img) {
+		super(xPos, yPos, img);
 	}
 
 	abstract void checkCollision(Character character);
