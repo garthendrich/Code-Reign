c85b23b664c9e7afb45d3a7802fee391c9e371e0
Alesundreau Dale Ratuiste <amratuiste@up.edu.ph>
2021-12-01 16:21:29 +0800

    Modified shoot() and move() in Character.java

--- src/game/Character.java
+++ src/game/Character.java
@@ -4,8 +4,6 @@ import java.util.ArrayList;
 import java.util.Random;
 import javafx.scene.image.Image;
 
-
-
 public class Character extends Sprite{
 	private String name;
 	private int strength;
@@ -17,41 +15,39 @@ public class Character extends Sprite{
 
 	public Character(String name, int x, int y){
 		super(x,y);
-		this.name = name;
 		Random r = new Random();
-		this.setStrength(r.nextInt(151)+100);
-		this.alive = true;
-		this.bullets = new ArrayList<Bullet>();
-		this.loadImage(Character.SHIP_IMAGE);
+		setStrength(r.nextInt(151)+100);
+		alive = true;
+		bullets = new ArrayList<Bullet>();
+		loadImage(Character.SHIP_IMAGE);
 	}
 
 	public boolean isAlive(){
-		if(this.alive) return true;
+		if(alive) return true;
 		return false;
 	}
 	public String getName(){
-		return this.name;
+		return name;
 	}
 
 	public void die(){
-    	this.alive = false;
+    	alive = false;
     }
 
 	//method that will get the bullets 'shot' by the ship
 	public ArrayList<Bullet> getBullets(){
-		return this.bullets;
+		return bullets;
 	}
 
 	//method called if spacebar is pressed
 	public void shoot(){
 		//compute for the x and y initial position of the bullet
-		int x = (int) (this.x + this.width+20);
-		int y = (int) (this.y + this.height/2);
+		int x =+ (int) ( width + 20);
+		int y =+ (int) ( height / 2);
 		/*
 		 * TODO: Instantiate a new bullet and add it to the bullets arraylist of ship
 		 */
-		Bullet newBullet = new Bullet(x, y);
-		this.bullets.add(newBullet);
+		this.bullets.add(new Bullet((int)(x + (width / 2)), y));
     }
 
 	//method called if up/down/left/right arrow key is pressed.
@@ -60,8 +56,12 @@ public class Character extends Sprite{
 		 *TODO: 		Only change the x and y position of the ship if the current x,y position
 		 *				is within the gamestage width and height so that the ship won't exit the screen
 		 */
-		this.x += this.dx;
-		this.y += this.dy;
+		if(x + dx >= 0 && x + dx <= GameStage.WINDOW_WIDTH - width){
+			x += dx;
+		}
+		if(y + dy >= 0 && y + dy <= GameStage.WINDOW_HEIGHT - height){
+			y += dy;
+		}
 	}
 
 	public int getStrength() {
