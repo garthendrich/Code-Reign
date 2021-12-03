d325f48bd2f3bfd19e84d56789245f63abf7f400
Alesundreau Dale Ratuiste <amratuiste@up.edu.ph>
2021-12-03 17:37:21 +0800

    Created methods based in UML

--- src/game/Sprite.java
+++ src/game/Sprite.java
@@ -1,5 +1,44 @@
 package game;
 
-public class Sprite {
+import javafx.scene.image.Image;
 
+public class Sprite extends GameElement{
+	protected final static int SPRITE_MOVING_DISTANCE = 10;
+	protected int dX, dY;
+
+	public Sprite(int xPos, int yPos, Image image) {
+		super(xPos, yPos, image);
+	}
+
+	void moveUp(){
+
+	}
+
+	void moveDown(){
+
+	}
+
+	void moveLeft(){
+
+	}
+
+	void moveRight(){
+
+	}
+
+	void stopMovingHorizontally(){
+
+	}
+
+	void stopMovingVertically(){
+
+	}
+
+	void setDX(int value){
+		this.dX = value;
+	}
+
+	void setDY(int value){
+		this.dY = value;
+	}
 }
