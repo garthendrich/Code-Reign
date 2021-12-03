77590fcecaf7648c4b083b311417adef92eb60cd
Alesundreau Dale Ratuiste <amratuiste@up.edu.ph>
2021-12-03 16:37:27 +0800

    Updated constructor

--- src/game/PowerUp.java
+++ src/game/PowerUp.java
@@ -1,11 +1,9 @@
 package game;
 
-import javafx.scene.image.Image;
-
 abstract class PowerUp extends GameElement{
 
-	public PowerUp(int xPos, int yPos, Image img) {
-		super(xPos, yPos, img);
+	public PowerUp(int xPos, int yPos) {
+		super(xPos, yPos);
 	}
 
 	abstract void checkCollision(Character character);
