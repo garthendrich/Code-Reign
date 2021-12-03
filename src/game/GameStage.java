947967fc910316b85b05521a314107e3a5bbed23
Garth Hendrich Lapitan <garthendrich@gmail.com>
2021-12-03 23:16:50 +0800

    Implement handling of scene key events

--- src/game/GameStage.java
+++ src/game/GameStage.java
@@ -1,9 +1,12 @@
 package game;
 
+import javafx.event.EventHandler;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
@@ -21,8 +24,28 @@ public class GameStage {
 		root.getChildren().add(canvas);
 		scene = new Scene(root, WINDOW_WIDTH, WINDOW_HEIGHT);
 
+		setupSceneEventHandlers();
+
 		GraphicsContext graphicsContext = canvas.getGraphicsContext2D();
 		gameTimer = new GameTimer(graphicsContext);
+	}
+
+	private void setupSceneEventHandlers() {
+		scene.setOnKeyPressed(new EventHandler<KeyEvent>() {
+			@Override
+			public void handle(KeyEvent event) {
+				KeyCode key = event.getCode();
+				gameTimer.handleKeyPress(key);
+			}
+		});
+
+		scene.setOnKeyReleased(new EventHandler<KeyEvent>() {
+			@Override
+			public void handle(KeyEvent event) {
+				KeyCode key = event.getCode();
+				gameTimer.handleKeyRelease(key);
+			}
+		});
 	}
 
 	/**
