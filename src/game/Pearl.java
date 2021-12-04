1cea93bacae5a41930eaeb4f1a9daf9bb64d4796
Garth Hendrich Lapitan <garthendrich@gmail.com>
2021-12-04 13:44:40 +0800

    Modify PowerUp subclasses constructors

--- src/game/Pearl.java
+++ src/game/Pearl.java
@@ -5,9 +5,8 @@ import javafx.scene.image.Image;
 class Pearl extends PowerUp{
 	private final static Image PEARL_IMAGE = new Image("images/biscuit.png");
 
-	Pearl(int x, int y, Image image){
-		super(x, y, image);
-		this.image = PEARL_IMAGE;
+	Pearl(int x, int y){
+		super(x, y, PEARL_IMAGE);
 	}
 
 	@Override
--- src/game/Star.java
+++ src/game/Star.java
@@ -5,9 +5,8 @@ import javafx.scene.image.Image;
 class Star extends PowerUp{
 	private final static Image STAR_IMAGE = new Image("images/biscuit.png");
 
-	Star(int x, int y, Image image){
-		super(x, y, image);
-		this.image = STAR_IMAGE;
+	Star(int x, int y){
+		super(x, y, STAR_IMAGE);
 	}
 
 	@Override
