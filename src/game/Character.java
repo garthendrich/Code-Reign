3edbfb7e4e972a0e535333eea6149d90a873b20b
Alesundreau Dale Ratuiste <amratuiste@up.edu.ph>
2021-12-04 10:10:36 +0800

    Modified methods based in UML

--- src/game/Character.java
+++ src/game/Character.java
@@ -5,34 +5,27 @@ import java.util.Random;
 import javafx.scene.image.Image;
 
 public class Character extends GameElement{
-	private String name;
 	private int strength;
 	private boolean alive;
 
 	private ArrayList<Bullet> bullets;
 	public final static Image CHARACTER_IMAGE = new Image("images/ship.png", Character.CHARACTER_WIDTH, Character.CHARACTER_WIDTH, false, false);
 	private final static int CHARACTER_WIDTH = 50;
+	private final static int MIN_STRENGTH = 100;
+	private final static int MAX_STRENGTH = 150;
 
-	public Character(String name, int x, int y){
-		super(x,y);
+	public Character(String name, int x, int y, Image image){
+		super(x,y, image);
 		Random r = new Random();
-		strength = r.nextInt(151)+100;
-		alive = true;
-		bullets = new ArrayList<Bullet>();
-		loadImage(Character.CHARACTER_IMAGE);
+		this.strength = r.nextInt((MAX_STRENGTH - MIN_STRENGTH) + 101);
+		this.alive = true;
+		this.bullets = new ArrayList<Bullet>();
 	}
 
 	public boolean isAlive(){
 		if(alive) return true;
 		return false;
 	}
-	public String getName(){
-		return name;
-	}
-
-	public void die(){
-    	alive = false;
-    }
 
 	//method that will get the bullets 'shot' by the ship
 	public ArrayList<Bullet> getBullets(){
@@ -42,34 +35,15 @@ public class Character extends GameElement{
 	//method called if spacebar is pressed
 	public void shoot(){
 		//compute for the x and y initial position of the bullet
-		int x =+ (int) ( width + 20);
-		int y =+ (int) ( height / 2);
+		int x =+ width + 20;
+		int y =+ height / 2;
 		/*
 		 * TODO: Instantiate a new bullet and add it to the bullets arraylist of ship
 		 */
-		this.bullets.add(new Bullet((int)(x + (width / 2)), y));
+		bullets.add(new Bullet(x + (width / 2), y));
     }
 
-	//method called if up/down/left/right arrow key is pressed.
-	public void move() {
-		/*
-		 *TODO: 		Only change the x and y position of the ship if the current x,y position
-		 *				is within the gamestage width and height so that the ship won't exit the screen
-		 */
-		if(x + dx >= 0 && x + dx <= GameStage.WINDOW_WIDTH - width){
-			x += dx;
-		}
-		if(y + dy >= 0 && y + dy <= GameStage.WINDOW_HEIGHT - height){
-			y += dy;
-		}
-	}
+	void reduceStrength(int amount){
 
-	public int getStrength() {
-		return strength;
 	}
-
-	public void addStrength(){
-		strength += 50;
-	}
-
 }
