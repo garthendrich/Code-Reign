393aa215bee3463e03a5414f90c62964837c7cc9
Alesundreau Dale Ratuiste <amratuiste@up.edu.ph>
2021-12-04 15:32:22 +0800

    Added health attribute

--- src/game/Agmatron.java
+++ src/game/Agmatron.java
@@ -5,8 +5,8 @@ import javafx.scene.image.Image;
 public class Agmatron extends Orglit {
 
 	public final static int AGMATRON_DAMAGE = 50;
-
-	private int health;
+	private final static int AGMATRON_HEALTH = 3000;
+	protected int health = AGMATRON_HEALTH;
 
 	public Agmatron(int x, int y) {
 		super(x, y);
@@ -15,7 +15,7 @@ public class Agmatron extends Orglit {
 	}
 
 	void reduceHealthBy(int amount) {
-		health -= amount;
+	health -= amount;
 	}
 
 	boolean isAlive() {
