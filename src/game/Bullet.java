87448087480889c66de85ca75a617a54e5cff6a6
Alesundreau Dale Ratuiste <amratuiste@up.edu.ph>
2021-12-04 10:15:20 +0800

    Modified based in UML

--- src/game/Bullet.java
+++ src/game/Bullet.java
@@ -3,26 +3,13 @@ package game;
 import javafx.scene.image.Image;
 
 public class Bullet extends GameElement {
-	private final static int BULLET_SPEED = 20;
+	private final static int BULLET_DAMAGE = 10;
 	private final static Image BULLET_IMAGE = new Image("images/bullet.png",Bullet.BULLET_WIDTH,Bullet.BULLET_WIDTH,false,false);
 	private final static int BULLET_WIDTH = 20;
 
-	public Bullet(int x, int y){
-		super(x,y);
-		loadImage(Bullet.BULLET_IMAGE);
+	public Bullet(int x, int y, Image image){
+		super(x,y, image);
 	}
 
 
-	//method that will move/change the x position of the bullet
-	public void move(){
-		/*
-		 * TODO: Change the x position of the bullet depending on the bullet speed.
-		 * 					If the x position has reached the right boundary of the screen,
-		 * 						set the bullet's visibility to false.
-		 */
-		x += Bullet.BULLET_SPEED;
-		if(x <= GameStage.WINDOW_WIDTH){
-			setVisible(false);
-		}
-	}
 }
