84ba1a5111b99f837b6da0c55566b3db9dbba6e6
Alesundreau Dale Ratuiste <amratuiste@up.edu.ph>
2021-12-04 12:35:03 +0800

    Rename Enemy to Orglit

--- src/game/Orglit.java
+++ src/game/Orglit.java
@@ -6,13 +6,14 @@ import java.util.Random;
 public class Orglit extends Sprite{
 	public int movementSpeed;
 	public static final int MAX_ORGLIT_SPEED = 5;
-	public final static Image ENEMY_IMAGE = new Image("images/fish.png", Orglit.WIDTH, Orglit.HEIGHT, false, false);
+	public final static Image ORGLIT_IMAGE = new Image("images/fish.png", Orglit.WIDTH, Orglit.HEIGHT, false, false);
 	public final static int WIDTH = 50;
 	public final static int HEIGHT = 50;
 	public final static int DAMAGE = 10;
 
 	public Orglit(int x, int y, Image image){
 		super(x,y, image);
+		this.image = ORGLIT_IMAGE;
 		Random r = new Random();
 		this.movementSpeed = r.nextInt(MAX_ORGLIT_SPEED) + 1;
 	}
