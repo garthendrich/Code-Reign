a1c28643d563f08dcd043702fb10cbb30dee7661
Alesundreau Dale Ratuiste <amratuiste@up.edu.ph>
2021-12-04 14:20:56 +0800

    Added PEARL_STRENGTH attribute

--- src/game/Pearl.java
+++ src/game/Pearl.java
@@ -4,6 +4,7 @@ import javafx.scene.image.Image;
 
 class Pearl extends PowerUp{
 	private final static Image PEARL_IMAGE = new Image("images/biscuit.png");
+	public final static int PEARL_STRENGTH = 50;
 
 	Pearl(int x, int y){
 		super(x, y, PEARL_IMAGE);
