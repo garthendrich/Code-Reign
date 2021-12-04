1775a2bc04b929632c33238195f011352be83a67
Alesundreau Dale Ratuiste <amratuiste@up.edu.ph>
2021-12-04 12:35:32 +0800

    Modified based in UML

--- src/game/Bullet.java
+++ src/game/Bullet.java
@@ -4,9 +4,11 @@ import javafx.scene.image.Image;
 
 public class Bullet extends Sprite{
 	public final static int BULLET_DAMAGE = 10;
+	public final static Image BULLET_IMAGE = new Image("images/bullet.png", 25, 25, false, false);
 
 	public Bullet(int x, int y, Image image){
 		super(x,y, image);
+		this.image = BULLET_IMAGE;
 	}
 
 }
