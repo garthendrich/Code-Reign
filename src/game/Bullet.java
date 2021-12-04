68eaf4d3f1cdf7fff3d8be5280e57a18647cf197
Garth Hendrich Lapitan <garthendrich@gmail.com>
2021-12-04 13:47:07 +0800

    Create Bullet damage attribute

--- src/game/Bullet.java
+++ src/game/Bullet.java
@@ -3,12 +3,18 @@ package game;
 import javafx.scene.image.Image;
 
 public class Bullet extends Sprite{
-	public final static int BULLET_DAMAGE = 10;
+
 	public final static Image BULLET_IMAGE = new Image("images/bullet.png", 25, 25, false, false);
 
-	public Bullet(int x, int y, Image image){
-		super(x,y, image);
-		this.image = BULLET_IMAGE;
+	private int damage;
+
+	public Bullet(int x, int y, int damage){
+		super(x,y, BULLET_IMAGE);
+
+		this.damage = damage;
 	}
 
+	int getDamage() {
+		return damage;
+	}
 }
