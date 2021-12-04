de4263a0258982442018d75d16d4e3dd2c46d65e
Garth Hendrich Lapitan <garthendrich@gmail.com>
2021-12-04 13:48:41 +0800

    Modify Edolite shoot()

--- src/game/Edolite.java
+++ src/game/Edolite.java
@@ -5,11 +5,13 @@ import java.util.Random;
 import javafx.scene.image.Image;
 
 public class Edolite extends Sprite{
-	private int strength;
-	private ArrayList<Bullet> bullets;
+
 	public final static Image EDOLITE_IMAGE = new Image("images/ship.png", 25, 25, false, false);
 	private final static int MIN_STRENGTH = 100;
 	private final static int MAX_STRENGTH = 150;
+
+	private int strength;
+	private ArrayList<Bullet> bullets;
 
 	public Edolite(int x, int y) {
 		super(x,y, EDOLITE_IMAGE);
@@ -26,12 +28,11 @@ public class Edolite extends Sprite{
 	//method called if spacebar is pressed
 	public void shoot(){
 		//compute for the x and y initial position of the bullet
-		int x =+ width + 20;
-		int y =+ height / 2;
-		/*
-		 * TODO: Instantiate a new bullet and add it to the bullets arraylist of ship
-		 */
-		bullets.add(new Bullet(x + (width / 2), y, Bullet.BULLET_IMAGE));
+		int bulletXPos =+ this.xPos + this.width + 16;
+		int bulletYPos =+ this.yPos + (this.height / 2);
+
+		Bullet bullet = new Bullet(bulletXPos, bulletYPos, strength);
+		bullets.add(bullet);
     }
 
 	int reduceStrength(int amount){
