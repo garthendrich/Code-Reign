54db1ab480fa53a9b8d46008fb0ed771cf19c952
Alesundreau Dale Ratuiste <amratuiste@up.edu.ph>
2021-12-04 13:36:15 +0800

    Added getAgmatronDamage()

--- src/game/Agmatron.java
+++ src/game/Agmatron.java
@@ -5,6 +5,7 @@ import javafx.scene.image.Image;
 public class Agmatron extends Orglit {
 	private final static Image AGMATRON_IMAGE = new Image("images/poro.png");
 	private int health;
+	public final static int AGMATRON_DAMAGE = 50;
 
 	public Agmatron(int x, int y) {
 		super(x, y, AGMATRON_IMAGE);
@@ -20,5 +21,10 @@ public class Agmatron extends Orglit {
 		}else{
 			return false;
 		}
+	}
+
+	public static int getAgmatronDamage(){
+		return AGMATRON_DAMAGE;
+
 	}
 }
