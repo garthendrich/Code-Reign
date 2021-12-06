cd5c6256497377d16fce8f2464c97e0428bfa4dc
Alesundreau Dale Ratuiste <amratuiste@up.edu.ph>
2021-12-06 17:15:32 +0800

    Removed unused import

--- src/game/GameTimer.java
+++ src/game/GameTimer.java
@@ -9,7 +9,6 @@ import javafx.scene.image.Image;
 import javafx.scene.input.KeyCode;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
-import javafx.scene.text.FontWeight;
 import javafx.scene.text.TextAlignment;
 
 class GameTimer extends AnimationTimer {
