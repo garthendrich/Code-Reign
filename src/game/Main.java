00b26cc42b4e7814a480218ee20e55f63e99e253
Alesundreau Dale Ratuiste <amratuiste@up.edu.ph>
2021-12-07 09:38:29 +0800

    Set the screen to permanent size

--- src/game/Main.java
+++ src/game/Main.java
@@ -8,6 +8,7 @@ public class Main extends Application {
 	public void start(Stage stage){
 		GameStage theGameStage = new GameStage();
 		theGameStage.loadTo(stage);
+		stage.setResizable(false);
 	}
 
 	public static void main(String[] args) {
