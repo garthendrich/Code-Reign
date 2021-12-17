056a1c08431cd1c01364e10cf9d720c29da4bcef
Alesundreau Dale Ratuiste <amratuiste@up.edu.ph>
2021-12-17 19:52:11 +0800

    Added background color

--- src/views/GameOverView.java
+++ src/views/GameOverView.java
@@ -2,10 +2,13 @@ package views;
 
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.scene.text.Text;
-
+import javafx.scene.text.TextAlignment;
 import main.Main;
 
 public class GameOverView extends View {
@@ -21,6 +24,7 @@ public class GameOverView extends View {
 	@Override
 	protected Scene createScene() {
 		VBox root = new VBox();
+		root.setBackground(new Background(new BackgroundFill(Color.valueOf("F6C27D"), null, null)));
 		root.setAlignment(Pos.CENTER);
 		root.setSpacing(8);
 
@@ -29,10 +33,11 @@ public class GameOverView extends View {
 		if (edoliteStrength >= 0) outComeMessageText = new Text("CONGRATULATIONS! YOU WIN!");
 		else outComeMessageText = new Text("GAME OVER! YOU LOSE!");
 
-		outComeMessageText.setFont(Font.font(Main.NOTALOT60, 48));
+		outComeMessageText.setFont(Font.font(Main.NOTALOT60, 50));
 
 		Text orglitsKilledText = new Text("Orglits killed: " + orglitsKilled);
-		orglitsKilledText.setFont(Font.font(Main.NOTALOT60, 32));
+		orglitsKilledText.setFont(Font.font(Main.NOTALOT60, 30));
+		orglitsKilledText.setTextAlignment(TextAlignment.CENTER);
 
 		root.getChildren().addAll(outComeMessageText, orglitsKilledText);
 
