f36a5040c0f0746e7c1464342bcea965be66c648
Alesundreau Dale Ratuiste <amratuiste@up.edu.ph>
2021-12-17 19:31:28 +0800

    Added instructions text

--- src/views/InstructionsView.java
+++ src/views/InstructionsView.java
@@ -2,6 +2,7 @@ package views;
 
 import javafx.event.EventHandler;
 import javafx.geometry.Pos;
+import javafx.geometry.VPos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.input.MouseEvent;
@@ -9,7 +10,9 @@ import javafx.scene.layout.Background;
 import javafx.scene.layout.BackgroundFill;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
 import javafx.scene.text.Text;
+import javafx.scene.text.TextAlignment;
 import javafx.stage.Stage;
 
 import main.Main;
@@ -21,14 +24,27 @@ class InstructionsView extends View {
 		VBox root = new VBox();
 		root.setBackground(new Background(new BackgroundFill(Color.valueOf("F6C27D"), null, null)));
 		root.setAlignment(Pos.CENTER);
-		root.setSpacing(8);
+		root.setSpacing(75);
 
 		Text instructionsTitle = new Text();
 		instructionsTitle.setText("Instructions");
+		instructionsTitle.setFont(Font.font(Main.NOTALOT60, 50));
+		instructionsTitle.setTextOrigin(VPos.TOP);
+
+		Text instructions = new Text();
+		instructions.setText("Press WASD to move the Edolite. \n "
+				+ "Press Spacebar to shoot. \n "
+				+ "Avoid colliding with enemies. \n "
+				+ "Pickup power-ups to help you survive. \n "
+				+ "To win the game, you must survive for 60 seconds.");
+		instructions.setFont(Font.font(Main.NOTALOT60, 30));
+		instructions.setTextAlignment(TextAlignment.CENTER);
+		instructions.setTextOrigin(VPos.CENTER);
 
 		Button backButton = new Button("Return to Main Menu");
+		backButton.setAlignment(Pos.BOTTOM_CENTER);
 
-		root.getChildren().addAll(instructionsTitle, backButton);
+		root.getChildren().addAll(instructionsTitle, instructions, backButton);
 
 		backButton.setOnMouseClicked(
 			new EventHandler<MouseEvent>() {
