6273f4dc18ecc6b6d970ac87c835b2a40e2de89f
Garth Hendrich Lapitan <garthendrich@gmail.com>
2021-12-19 01:54:28 +0800

    Add PowerUp docs

--- src/game/PowerUp.java
+++ src/game/PowerUp.java
@@ -10,5 +10,10 @@ abstract class PowerUp extends Sprite {
 		super(xPos, yPos, image);
 	}
 
+	/**
+	 * Applies a particular buff to the given edolite.
+	 *
+	 * @param edolite The edolite that will obtain the buff.
+	 */
 	abstract void applyTo(Edolite edolite);
 }
