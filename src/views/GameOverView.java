78e84f1da3cff30a663ecb06269adf22a577b227
Alesundreau Dale Ratuiste <amratuiste@up.edu.ph>
2021-12-18 11:09:46 +0800

    Added return to menu button

--- src/views/GameOverView.java
+++ src/views/GameOverView.java
@@ -1,7 +1,10 @@
 package views;
 
+import javafx.event.EventHandler;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.BackgroundFill;
 import javafx.scene.layout.VBox;
@@ -9,6 +12,7 @@ import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 import javafx.scene.text.TextAlignment;
+import javafx.stage.Stage;
 import main.Main;
 
 public class GameOverView extends View {
@@ -26,7 +30,7 @@ public class GameOverView extends View {
 		VBox root = new VBox();
 		root.setBackground(new Background(new BackgroundFill(Color.valueOf("F6C27D"), null, null)));
 		root.setAlignment(Pos.CENTER);
-		root.setSpacing(8);
+		root.setSpacing(50);
 
 		Text outComeMessageText;
 
@@ -39,7 +43,22 @@ public class GameOverView extends View {
 		orglitsKilledText.setFont(Font.font(Main.NOTALOT60, 30));
 		orglitsKilledText.setTextAlignment(TextAlignment.CENTER);
 
-		root.getChildren().addAll(outComeMessageText, orglitsKilledText);
+		Button backButton = new Button("Return to Main Menu");
+		backButton.setAlignment(Pos.BOTTOM_CENTER);
+
+		root.getChildren().addAll(outComeMessageText, orglitsKilledText, backButton);
+
+		backButton.setOnMouseClicked(
+			new EventHandler<MouseEvent>() {
+				@Override
+				public void handle(MouseEvent event) {
+					Stage stage = getStage();
+
+					TitleView titleView = new TitleView();
+					titleView.loadTo(stage);
+				}
+			}
+		);
 
 		return new Scene(root, Main.WINDOW_WIDTH, Main.WINDOW_HEIGHT);
 	}
